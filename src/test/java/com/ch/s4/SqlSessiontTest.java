dcb40759b0de891eb0d354a93412c028107ea820
MYCHCH515 <nam1738@naver.com>
2020-10-21 17:49:17 +0900

    pre setting

---
+++ src/test/java/com/ch/s4/SqlSessiontTest.java
@@ -0,0 +1,20 @@
+package com.ch.s4;
+
+import static org.junit.Assert.*;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SqlSessiontTest extends MyTestCase{
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	
+	@Test
+	public void test() {
+		assertNotNull(sqlSession);
+	}
+
+}
